36ecb252b4e2ecf5417af03ee72e98e3aad3b51f
Jared Immerman <jimmerman819@gmail.com>
2024-11-15 19:42:56 -0500

    temp

--- LST/server/webScraper/webScraper.js
+++ LST/server/webScraper/webScraper.js
@@ -11,14 +11,14 @@ async function scrapeData() {
   await page.waitForNetworkIdle();
 
   const data = await page.evaluate(() => {
-    const elements = document.querySelectorAll(".champion-name");
     const winRates = document.querySelectorAll(".winrate");
 
-    const result = {};
-    elements.forEach((element, index) => {
-      const championName = element.textContent;
-      const championWinRate = winRates[index + 1].textContent;
-      result[championName] = championWinRate;
+    const result = [];
+    winRates.forEach((winRateElement, index) => {
+      if (index > 0) {
+        // Skip the first item if it's a header
+        result.push(winRateElement.textContent);
+      }
     });
 
     return result;
@@ -55,3 +55,60 @@ async function autoScroll(page, maxScrolls) {
 }
 
 scrapeData();
+// const puppeteer = require("puppeteer");
+
+// async function scrapeData() {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   await page.goto("https://u.gg/lol/tier-list");
+
+//   await autoScroll(page);
+
+//   await page.waitForNetworkIdle();
+
+//   const data = await page.evaluate(() => {
+//     const elements = document.querySelectorAll(".champion-name");
+//     const winRates = document.querySelectorAll(".winrate");
+
+//     const result = {};
+//     elements.forEach((element, index) => {
+//       const championName = element.textContent;
+//       const championWinRate = winRates[index + 1].textContent;
+//       result[championName] = championWinRate;
+//     });
+
+//     return result;
+//   });
+
+//   console.log(data);
+
+//   await browser.close();
+// }
+
+// async function autoScroll(page, maxScrolls) {
+//   await page.evaluate(async (maxScrolls) => {
+//     await new Promise((resolve) => {
+//       var totalHeight = 0;
+//       var distance = 100;
+//       var scrolls = 0; // scrolls counter
+//       var timer = setInterval(() => {
+//         var scrollHeight = document.body.scrollHeight;
+//         window.scrollBy(0, distance);
+//         totalHeight += distance;
+//         scrolls++; // increment counter
+
+//         // stop scrolling if reached the end or the maximum number of scrolls
+//         if (
+//           totalHeight >= scrollHeight - window.innerHeight ||
+//           scrolls >= maxScrolls
+//         ) {
+//           clearInterval(timer);
+//           resolve();
+//         }
+//       }, 100);
+//     });
+//   }, maxScrolls); // pass maxScrolls to the function
+// }
+
+// scrapeData();
