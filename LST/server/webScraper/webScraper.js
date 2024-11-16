dbc408685dbf3b65af5983fe75d368931fd4cdbb
Jared Immerman <jimmerman819@gmail.com>
2024-11-15 19:08:40 -0500

    temp

--- LST/server/webScraper/webScraper.js
+++ LST/server/webScraper/webScraper.js
@@ -1,25 +1,25 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 async function scrapeData() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://u.gg/lol/tier-list');
+  await page.goto("https://u.gg/lol/tier-list");
 
   const data = await page.evaluate(() => {
-    const elements = document.querySelectorAll('.champion-name');
-    const winRates = document.querySelectorAll('.winrate');
-    
+    const champs = document.querySelectorAll(".champion-name");
+    // const winRates = document.querySelectorAll(".winrate");
+
     const result = {};
-    elements.forEach((element, index) => {
-      const championName = element.textContent;
-      const championWinRate =  winRates[index+1].textContent;
-      result[championName] = championWinRate;
+    champs.forEach((champElement, index) => {
+      const championName = champElement.textContent;
+      console.log(championName);
+      // const championWinRate =  winRates[index+1].textContent;
+      result[championName] = championName;
     });
-  
+
     return result;
   });
-  
 
   console.log(data);
 
