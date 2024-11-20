8d4f134ac2e5506b48fbd4fee2123c4d0dca6cf4
Jared Immerman <jimmerman819@gmail.com>
2024-11-20 13:30:09 -0500

    completed champion webscrape

--- LST/server/webScraper/webScraper.js
+++ LST/server/webScraper/webScraper.js
@@ -4,13 +4,12 @@ async function scrapeData() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto("https://u.gg/lol/tier-list");
+  await page.goto("https://u.gg/lol/tier-list", { timeout: 60000 });
 
   await autoScroll(page);
-
   await page.waitForNetworkIdle();
 
-  const data = await page.evaluate(() => {
+  const winData = await page.evaluate(() => {
     const winRates = document.querySelectorAll(".winrate");
 
     const result = [];
@@ -24,7 +23,19 @@ async function scrapeData() {
     return result;
   });
 
-  console.log(data);
+  const charData = await page.evaluate(() => {
+    const champions = document.querySelectorAll(".champion-name");
+
+    const result = [];
+    champions.forEach((champElement, index) => {
+      result.push(champElement.textContent);
+    });
+
+    return result;
+  });
+
+  console.log(winData);
+  console.log(charData);
 
   await browser.close();
 }
@@ -55,60 +66,3 @@ async function autoScroll(page, maxScrolls) {
 }
 
 scrapeData();
-// const puppeteer = require("puppeteer");
-
-// async function scrapeData() {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   await page.goto("https://u.gg/lol/tier-list");
-
-//   await autoScroll(page);
-
-//   await page.waitForNetworkIdle();
-
-//   const data = await page.evaluate(() => {
-//     const elements = document.querySelectorAll(".champion-name");
-//     const winRates = document.querySelectorAll(".winrate");
-
-//     const result = {};
-//     elements.forEach((element, index) => {
-//       const championName = element.textContent;
-//       const championWinRate = winRates[index + 1].textContent;
-//       result[championName] = championWinRate;
-//     });
-
-//     return result;
-//   });
-
-//   console.log(data);
-
-//   await browser.close();
-// }
-
-// async function autoScroll(page, maxScrolls) {
-//   await page.evaluate(async (maxScrolls) => {
-//     await new Promise((resolve) => {
-//       var totalHeight = 0;
-//       var distance = 100;
-//       var scrolls = 0; // scrolls counter
-//       var timer = setInterval(() => {
-//         var scrollHeight = document.body.scrollHeight;
-//         window.scrollBy(0, distance);
-//         totalHeight += distance;
-//         scrolls++; // increment counter
-
-//         // stop scrolling if reached the end or the maximum number of scrolls
-//         if (
-//           totalHeight >= scrollHeight - window.innerHeight ||
-//           scrolls >= maxScrolls
-//         ) {
-//           clearInterval(timer);
-//           resolve();
-//         }
-//       }, 100);
-//     });
-//   }, maxScrolls); // pass maxScrolls to the function
-// }
-
-// scrapeData();
